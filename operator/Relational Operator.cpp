5ad602f99f02e94b6f58a59af7ea1ff1f4fa6f53
Shivansh Mishra <131871626+ishivansmishra@users.noreply.github.com>
2024-06-10 00:44:54 +0530

    Relational Operator.cpp

---
+++ operator/Relational Operator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main (){
+  int a;
+  cout<<"Enter The First number : "<<endl;
+  cin>>a;
+
+  int b;
+  cout<<"Enter The Second number : "<<endl;
+  cin>>b;
+
+  cout<<"The Relational operator is : "<<(a==b)<<endl;
+  cout<<"The Relational operator is : "<<(a>b)<<endl;
+  cout<<"The Relational operator is : "<<(a<b)<<endl;
+  cout<<"The Relational operator is : "<<(a!=b)<<endl;
+  cout<<"The Relational operator is : "<<(a>=b)<<endl;
+  cout<<"The Realational operator is : "<<(a>=b)<<endl;
+
+  return 0;
+
+
+}
