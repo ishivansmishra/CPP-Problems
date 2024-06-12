402ffebe34b2559182fde7926ecb03f2d695964a
Shivansh Mishra <131871626+ishivansmishra@users.noreply.github.com>
2024-06-12 14:26:30 +0530

    Create Basic Input output problem.cpp

---
+++ Basic Input output problem.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main () {
+  string name;
+  cout<<"Enter The Student Name  : "<<endl;
+  cin>>name;
+
+  int age;
+  cout<<"Enter The Student Age :  "<<endl;
+  cin>>age;
+
+  cout<<"The Name is : "<<name<<" "<<"The Age is : "<<age<<endl;
+
+  return 0;
+}
+
